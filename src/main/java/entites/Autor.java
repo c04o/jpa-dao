fc340e52ada7e7a7322a6c2fddf67f693027ce29
c04o <c04o@proton.me>
2025-10-22 17:50:03 -0600

    Update Autor class

--- src/main/java/entites/Autor.java
+++ src/main/java/entites/Autor.java
@@ -1,4 +1,27 @@
 package entites;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "autores")
+@Getter
+@Setter
+@NamedQueries({
+        @NamedQuery(name = "autores.All",
+        query = "select a from Autor a")
+})
+
 public class Autor {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "nombre_autor", length = 60, nullable = false)
+    private String nombre;
+    @Column(name = "nacionalidad", length = 60, nullable = false)
+    private String nacionalidad;
+    
+    private localDate fechaNac;
 }
