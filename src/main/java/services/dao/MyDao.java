1e8564d8992bb29da5ac074c73c019a7c2be5b2b
c04o <c04o@proton.me>
2025-10-22 18:08:19 -0600

    MyDao implements ICRUD

--- src/main/java/services/dao/MyDao.java
+++ src/main/java/services/dao/MyDao.java
@@ -1,4 +1,19 @@
 package services.dao;
 
-public class MyDao {
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import util.JPAConexion;
+
+public class MyDao implements ICRUD {
+    @Override
+    public <T> List<T> getAll(String nameQuery, Class<T> clazz) {
+        EntityManager em = JPAConexion.getEntityManager();
+        try {
+            TypedQuery<T> query = em.createNamedQuery(nameQuery, clazz);
+            return query.getResultList();
+        } catch (Exception ex){ex.printStackTrace();}
+        finally{em.close();}
+        return null;
+    }
 }
