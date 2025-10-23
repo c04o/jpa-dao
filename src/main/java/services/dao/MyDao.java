ae8d49f3f5b8a9e07c04020ba18a06166cf05ba8
c04o <c04o@proton.me>
2025-10-22 18:46:15 -0600

    fix my everything

--- src/main/java/services/dao/MyDao.java
+++ src/main/java/services/dao/MyDao.java
@@ -1,34 +1,52 @@
 package services.dao;
 
-import jakarta.persistence.*;
-import lombok.Getter;
-import lombok.Setter;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.TypedQuery;
+import services.interfaces.ICRUD;
 import util.JPAConexion;
 
+import java.util.List;
+
 public class MyDao implements ICRUD {
+
+
     @Override
     public <T> List<T> getAll(String nameQuery, Class<T> clazz) {
         EntityManager em = JPAConexion.getEntityManager();
-        try {
+        try{
             TypedQuery<T> query = em.createNamedQuery(nameQuery, clazz);
             return query.getResultList();
-        } catch (Exception ex){ex.printStackTrace();}
+        }catch(Exception ex){ex.printStackTrace();}
         finally{em.close();}
         return null;
     }
+
     @Override
     public <T> void insert(T entity) {
         EntityManager em = JPAConexion.getEntityManager();
-        try {
+        try{
             em.getTransaction().begin();
             em.persist(entity);
             em.getTransaction().commit();
-        } catch (Exception ex){
+        }catch(Exception ex){
             ex.printStackTrace();
             em.getTransaction().rollback();
         }
         finally{em.close();}
     }
+
+    @Override
+    public <T> void update(T entity) {
+        EntityManager em = JPAConexion.getEntityManager();
+        try{
+            em.getTransaction().begin();
+            em.merge(entity);
+            em.getTransaction().commit();
+        }catch(Exception ex){ex.printStackTrace();
+            em.getTransaction().rollback();}
+        finally{em.close();}
+    }
+
     @Override
     public <T> void delete(T entity) {
         EntityManager em = JPAConexion.getEntityManager();
@@ -36,18 +54,23 @@ public class MyDao implements ICRUD {
             em.getTransaction().begin();
             em.remove(em.merge(entity));
             em.getTransaction().commit();
-        } catch (Exception ex){ex.printStackTrace();
-        em.getTransaction().rollback();}
+        }catch(Exception ex){ex.printStackTrace();
+            em.getTransaction().rollback();}
         finally{em.close();}
+
     }
+
     @Override
     public <T> T findById(Integer id, Class<T> clazz) {
         EntityManager em = JPAConexion.getEntityManager();
-        try {
+        try{
             T entity = em.find(clazz, id);
             return entity;
-        } catch (Exception ex){ex.printStackTrace();}
+        }catch(Exception ex){ex.printStackTrace();}
         finally{em.close();}
         return null;
     }
 }
+
+
+//
