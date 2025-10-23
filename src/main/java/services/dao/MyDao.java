a4e9f7f4734b0887f9e2021fcc051ff9db0bacb9
c04o <c04o@proton.me>
2025-10-22 18:14:49 -0600

    Update MyDao Overrides

--- src/main/java/services/dao/MyDao.java
+++ src/main/java/services/dao/MyDao.java
@@ -29,4 +29,25 @@ public class MyDao implements ICRUD {
         }
         finally{em.close();}
     }
+    @Override
+    public <T> void delete(T entity) {
+        EntityManager em = JPAConexion.getEntityManager();
+        try{
+            em.getTransaction().begin();
+            em.remove(em.merge(entity));
+            em.getTransaction().commit();
+        } catch (Exception ex){ex.printStackTrace();
+        em.getTransaction().rollback();}
+        finally{em.close();}
+    }
+    @Override
+    public <T> T findById(Integer id, Class<T> clazz) {
+        EntityManager em = JPAConexion.getEntityManager();
+        try {
+            T entity = em.find(clazz, id);
+            return entity;
+        } catch (Exception ex){ex.printStackTrace();}
+        finally{em.close();}
+        return null;
+    }
 }
